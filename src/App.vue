86aaadac7ee4e5e34d02b248886f320c10abd488
kifuan <kifuan@foxmail.com>
2023-02-11 17:48:03 +0800

    chore: init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import Helize from './components/Helize.vue'
+</script>
+
+<template>
+  <Helize />
+</template>
+
+<style>
+#app {
+  margin: 3rem;
+  padding-top: 2rem;
+}
+</style>
---
+++ src/components/Helize.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { NForm, NFormItem, NInput, NH1 } from 'naive-ui'
+import { computed, ref } from 'vue'
+
+const num = ref('0')
+const output = computed(() => String(Number(num.value) * 10))
+</script>
+
+<template>
+  <NH1>Helize</NH1>
+  <NForm label-placement="left" label-width="auto" size="large">
+    <NFormItem label="Number">
+      <NInput v-model:value="num" />
+    </NFormItem>
+    <NFormItem label="Output">
+      <NInput readonly :value="output" />
+    </NFormItem>
+  </NForm>
+</template>
